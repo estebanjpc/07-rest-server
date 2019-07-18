83315f53ac4af751a01984666964430dfd81968d
Esteban Perez Cardenas <esteban.perez@difarma.cl>
2019-07-18 17:04:53 -0400

    ejemplos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,40 +8,16 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-})
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err,res)=>{
+    if (err) throw err;
 
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }else{
-        res.json({
-            body
-        });
-    }
-
-
-  })
-
-app.put('/usuario/:id', function (req, res) {
-let id = req.params.id;
-
-res.json({
-    id
+    console.log('Base de Datos ONLINE');
+    
 });
-})
- 
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-    })
 
 app.listen(process.env.PORT,()=>{
     console.log('escuchando puerto: 3000');
