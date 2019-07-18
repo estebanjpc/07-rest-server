dbd00592333aaccc73f0f828dac2719d49606bd2
Esteban Perez Cardenas <esteban.perez@difarma.cl>
2019-07-18 18:35:43 -0400

    ejerciios fail

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
  
 
-mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err,res)=>{
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true, useCreateIndex: true}, (err,res)=>{
     if (err) throw err;
 
     console.log('Base de Datos ONLINE');
