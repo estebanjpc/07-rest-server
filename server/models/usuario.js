f22e510276c16c7770b6cfbdb6f0ad89dbcebe19
Esteban Perez Cardenas <esteban.perez@difarma.cl>
2019-07-22 18:46:34 -0400

    JOB-subir heroku con varias cosas

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
+
 
 let Schema = mongoose.Schema;
 
-let rolesValidos = {
-    values: ['ADMIN_ROLE','USER_ROLE'],
-    message: '{VALUE} no es un rol valido'
-};
 
-let  usuarioSchema = new Schema({
+let usuarioSchema = new Schema({
     nombre: {
         type: String,
-        required: [true,'El nombre es necesario']
+        required: [true, 'El nombre es necesario']
     },
     email: {
         type: String,
@@ -25,7 +26,8 @@ let  usuarioSchema = new Schema({
         required: [true, 'La contraseña es obligatoria']
     },
     img: {
-        type: String
+        type: String,
+        required: false
     },
     role: {
         type: String,
@@ -42,15 +44,18 @@ let  usuarioSchema = new Schema({
     }
 });
 
-usuarioSchema.methods.toJSON = function(){
+
+usuarioSchema.methods.toJSON = function() {
+
     let user = this;
     let userObject = user.toObject();
-    delete userObject.password
+    delete userObject.password;
+
     return userObject;
 }
 
-usuarioSchema.plugin(uniqueValidator,{
-    message: '{PATH} debe ser unico'
-});
 
-module.exports = mongoose.model('Usuario',usuarioSchema);
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,24 +1,34 @@
 require('./config/config');
+
 const express = require('express');
 const mongoose = require('mongoose');
+
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json());
-app.use(require('./routes/usuario'));
- 
 
-mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true, useCreateIndex: true}, (err,res)=>{
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(require('./routes/usuario'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
     if (err) throw err;
 
-    console.log('Base de Datos ONLINE');
-    
+    console.log('Base de datos ONLINE');
+
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log('escuchando puerto: 3000');
-})
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
